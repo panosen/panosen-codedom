66ada9fecbe8ccfc4920c92110a06bb4293787ec
harriszhang <harriszhang@live.cn>
2021-06-10 14:20:59 +0800

    update *.bat

--- 0 - Build-Publish-Move.bat
+++ 0 - Build-Publish-Move.bat
@@ -4,8 +4,6 @@ dotnet restore
 
 dotnet build --no-restore -c Release
 
-dotnet nuget push Panosen.CodeDom\bin\Release\Panosen.CodeDom.*.nupkg -s https://package.savory.cn/v3/index.json --skip-duplicate
-
 move /Y Panosen.CodeDom\bin\Release\Panosen.CodeDom.*.nupkg D:\LocalSavoryNuget\
 
 pause
